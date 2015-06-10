cb3be32cf335c410f0d42b0701b7bd70f5b5cbf3
rizza <rizzapunsalan@yahoo.com.ph>
2015-06-10 16:51:25 +0800

    Initial commit

---
+++ CustomFacebookAPI/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  CustomFacebookAPI
+//
+//  Created by Rizza on 6/10/15.
+//  Copyright (c) 2015 Rizza Corella Punsalan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomFacebookAPI/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  CustomFacebookAPI
+//
+//  Created by Rizza on 6/10/15.
+//  Copyright (c) 2015 Rizza Corella Punsalan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
